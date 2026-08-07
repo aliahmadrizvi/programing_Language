cd500be3877384f42b77641b46f646580edf3b58
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-08-07 20:53:14 +0530

    started java

---
+++ java/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 19;
+        double cpga = 9.05;
+        char grade = 'A';
+        boolean IsStudent = true;
+        String name = "Ali";
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CPGA : " + cpga);
+        System.out.println("Grade : " + grade);
+        System.out.println("Student : " + IsStudent);
+
+    }
+
+}
