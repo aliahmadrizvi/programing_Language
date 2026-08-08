753b3e77d126e6072a5ebcb2d265a16f305169f6
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-08-08 18:46:13 +0530

    ques done loops

---
+++ Javascript/loops.js
@@ -0,0 +1,123 @@
+// print numbers from1 to 10 using for loop;
+
+
+// for(let i = 1; i<11;i++){
+//     console.log(i);
+// }
+
+// 2print numbers from10 to 1using while loop;
+
+// let i =10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+//3 print even number s from to 1 to 20 
+
+// for(let i = 1;i<21;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+//4 print odd number 1 to 15
+
+// let i = 1;
+// while(i<16){
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//5print table of 5 
+// for(let i = 1;i<11;i++){
+//     let ans = 5*i
+//     console.log(`5 X ${i} = ${ans}`);
+// }
+
+//6 print sum a loop 1 to 100
+// let sum = 0;
+
+// for(let i = 1;i<100;i++){
+//     sum+=i;
+    
+// }
+// console.log(sum);
+
+//7print all num which divisbble by 3 1 to 50
+// for(let i = 1;i<50;i++){
+//     if(i%3===0){
+//         console.log(i);
+//     }
+// }
+
+
+//8 ask user to enter a number btw 1 to 100 and  outpu will be odd and even number
+
+// let val = prompt("enter value btw 1 to 100: ");
+
+// for(let i=1;i<100;i++){
+//     if(val%2===0){
+//         console.log(`user number is ${val} is even`);
+//     }
+//     else{
+//          console.log(`user number is ${val} is odd`);
+//     }
+// }
+
+//9 check number divible by 3 and 5 in 1 to 100
+// for(let i = 1;i<100;i++){
+//     if(i%3===0 && i%5===0){
+//         console.log(i);
+//     }
+// }
+
+//10 loops go 1 to 100 find first multiple of 7 stops there
+// for(let i = 1;i<101;i++){
+//     console.log(i);
+//     if(i%7===0){
+        
+//         break
+//     }
+    
+// }
+
+//11 write a loop 1to 20 skips multipla of 3 other number 
+// for(let i = 1;i<21;i++){
+    
+//     if(i%3===0){
+        
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+//12
+// let count = 0;
+// for(let i = 1;i<101;i++){
+    
+//     if(i%3===0){
+//         count++;
+//     }
+    
+    
+// }
+// console.log(count);
+//13
+let sum = 0;
+for(let i = 1;i<101;i++){
+    
+    if(i%2===0){
+        sum+=i;
+    }
+    
+    
+}
+console.log(sum);
+
+
+
+
