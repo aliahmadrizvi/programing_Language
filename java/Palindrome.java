4a9052a9ec9f7bff4a8eb64bc48045f491df74b6
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-08-13 20:24:54 +0530

    Create Palindrome.java

---
+++ java/Palindrome.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int num = sc.nextInt();
+        int last, temp= num , rev=0;
+        while(temp>0){
+            last = temp%10;
+            rev=rev*10+last;
+            temp=temp/10;
+        }
+        if(rev==num){
+            System.out.println("Palindrome number");
+        }
+        else{
+            System.out.print("not a palindrome ");
+        }
+    }
+}
