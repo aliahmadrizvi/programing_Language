18ec39df74ecb80d81fed3b5d57c177db644879d
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-08-22 00:33:35 +0530

    js ques

---
+++ Javascript/DOM.js
@@ -0,0 +1,20 @@
+// let lis = document.querySelectorAll("li");
+// lis.forEach(function(val){
+//     console.log(val.textContent);
+
+// });
+// let para = document.querySelector("p");
+// para.innerHTML="<b>Updated</b> by javaScript";
+
+
+// let img = document.querySelector("img");
+// console.log(img.getAttribute("src"));
+
+// document.querySelector("img").setAttribute("src","https://i.pinimg.com/736x/b1/89/88/b189887090eb97fd555b02fe520ae984.jpg");
+
+
+
+let ul  = document.querySelector("ul");
+let lis  = document.querySelector("li");
+
+ul.removeChild(lis);
